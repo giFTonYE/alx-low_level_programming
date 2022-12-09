72a5f66f41670888b73d66cf7a777bced9c0fcd6
giFTonYE <giftOC66@gmail.com>
2022-12-09 03:17:22 -0800

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, butthe outcome isa piece of art,\n");
+       return (0);
+}       
