c0fe13fb4ac62f90b6f9e09d33619923a9d5a4fe
giFTonYE <giftOC66@gmail.com>
2022-12-10 09:56:09 -0800

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * FILE: 2-print_alphabet.c
+ * AUTH: giFTonYE
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower.
+ *
+ * Return: Always 0.
+ */int main(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('n\');
+
+	return (0);
+}
