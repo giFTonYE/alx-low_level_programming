8e9846f3a744575be3292437a1f07bfb28a02080
giFTonYE <giftOC66@gmail.com>
2022-12-19 06:49:50 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int -> given two integers swaap the values they are holding
+ * @a: paramete 1
+ * @b: parameter 2
+ */
+void swap_int(int 8a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
