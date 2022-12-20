d995007fae4a21b27933779d6d3dc9d101e6d71f
giFTonYE <giftOC66@gmail.com>
2022-12-20 05:44:42 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - generates random passwords for 101-crackme
+ * Return: zero
+ */
 int main(void)
 {
+	int sum;
+	char c;
 
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 	return (0);
 }
