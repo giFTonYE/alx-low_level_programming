fdb4f20294e0ca399f4f2cf3f0e76164c2b32f29
giFTonYE <giftoc66@gmail.com>
2023-01-06 05:51:45 -0800

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Description: return 1 if function is palindrome
+ * 	and 0 if function is not
+ * return(1)
+ */
+
+int is_palindrome(char *s);
+{
+a = pip
+b = peep
+c = dip
+
+	{
+	printf("%d", a);
+	printf("%d", b);
+	printf("%d", c);
+	}
+	return(1);
+}
