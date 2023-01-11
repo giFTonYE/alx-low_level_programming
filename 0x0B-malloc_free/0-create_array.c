92df2e68e8eb1507d7ce62f00665ecf3552ab20b
giFTonYE <giftoc66@gmail.com>
2023-01-11 05:51:30 -0800

    0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,7 @@ char *create_array(unsigned int size, char c)
 	{
 		array = (char *)malloc(size * sizeof(char));
 		if (array != NULL)
+
 		{																		for (i = 0; i < size; i++)															array[i] = c;
 		}
 	}
