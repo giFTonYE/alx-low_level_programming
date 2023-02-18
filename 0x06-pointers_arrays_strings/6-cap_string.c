0cb48e23479ed57701c364de7c2e15ee4e917d8d
giFTonYE <giftoc66@gmail.com>
2023-02-18 05:28:08 -0800

    pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include main.h"
 
 /**
  * cap_string - Write a function that capitalizes all words of a string.
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *n)
 {
 	int i, x;
-	int find[] = {'a', 'A', 'e', 'E', 'o','O', 't', 'T', 'l', 'L'};
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int replacer[] = {'4', '3', '0', '7', '1'};
 
 	for (i = 0; n[i] != '\0'; i++)
@@ -18,7 +18,7 @@ char *leet(char *n)
 		{
 			if (n[i] == find[x])
 			{
-				n[i] = replacer[x /2];
+				n[i] = replacer[x / 2];
 				x = 9;
 			}
 		}
